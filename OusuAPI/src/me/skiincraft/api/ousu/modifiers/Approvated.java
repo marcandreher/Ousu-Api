9490b5d308d1f9151ac03e107b424ef15a7bce43
Cristian Ferreira (SknZ) <cristian.datingaa@gmail.com>
2020-05-29 10:52:35 -0300

    Update Approvated.java

--- OusuAPI/src/me/skiincraft/api/ousu/modifiers/Approvated.java
+++ OusuAPI/src/me/skiincraft/api/ousu/modifiers/Approvated.java
@@ -2,7 +2,7 @@ package me.skiincraft.api.ousu.modifiers;
 
 public enum Approvated {
 	
-	Graveyard(-2), WIP(-1),Pending(0), Ranked(1), Approved(2), Qualified(3), Loved(4);
+	UNSPECIFIED(-3),Graveyard(-2), WIP(-1),Pending(0), Ranked(1), Approved(2), Qualified(3), Loved(4);
 
 	private int id;
 	Approvated(int id){
