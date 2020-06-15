8b48fcde71b48a2438f337d0c7945065bfaa41a1
Cristian Ferreira (SknZ) <cristian.datingaa@gmail.com>
2020-06-15 18:41:23 -0300

    Revert "Update UserBuilder.java"
    This reverts commit 07c667c66056c777d7bc0dd4c5456ffe4354a0c0.

--- src/main/java/me/skiincraft/api/ousu/users/UserBuilder.java
+++ src/main/java/me/skiincraft/api/ousu/users/UserBuilder.java
@@ -1,13 +1,9 @@
 package me.skiincraft.api.ousu.users;
 
-import java.io.IOException;
 import java.math.BigDecimal;
-import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-
-import javax.imageio.ImageIO;
 
 import com.github.kevinsawicki.http.HttpRequest;
 import com.google.gson.Gson;
@@ -181,13 +177,7 @@ public class UserBuilder {
 
 			@Override
 			public String getUserAvatar() {
-				try {
-					String url = "http://s.ppy.sh/a/" + user.getUser_id() + ".png";
-					ImageIO.read(new URL(url));
-					return url;
-				} catch (IOException e) {
-					return "https://i.imgur.com/tG1btnR.png";
-				}
+				return "http://s.ppy.sh/a/" + user.getUser_id() + ".png";
 			}
 
 			@Override
